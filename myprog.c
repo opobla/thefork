31e92d3b98760f76d1a3f7bb57b43c1b0217c74d
Óscar García <oscar.garcia@makingscience.com>
2021-11-03 13:21:57 +0100

    It a very big world ... so... whatever

--- myprog.c
+++ myprog.c
@@ -5,14 +5,14 @@
 
 int main() {
     int i,j,k;
-    printf("Hello big world! \n");
+    printf("Hello very big world! \n");
 
     i=fork();
     printf("A\n - %d",i);
     sleep(1);
     j=fork();
     printf("B\n - %d",j);
-    sleep(1);
+    sleep(2);
     k=fork();
     printf("D\n - %d",k);
     sleep(1);
